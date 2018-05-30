529535aa15160247baf480f048b478c8e2e86296
theerayut <theerayut.t@mono.co.th>
2018-05-30 16:39:22 +0700

    Set route

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,6 +6,7 @@
         <form @submit.prevent="enterChat">
           <label for="name">Enter Your name:</label>
           <input type="text" name="name" v-model="name">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
           <button class="btn teal">Enter Chat</button>
         </form>
       </div>
@@ -18,12 +19,18 @@ export default {
   name: 'Welcome',
   data () {
     return {
-      name: null
+      name: null,
+      feedback: null
     }
   },
   methods: {
     enterChat() {
-      console.log(this.name)
+      if (this.name) {
+        this.feedback = null 
+        this.$router.push({ name: 'Chat', params: { name: this.name } })
+      } else {
+        this.feedback = 'You must enter a name to join.'
+      }
     }
   }
 }
@@ -34,5 +41,11 @@ export default {
 .welcome {
   max-width: 400px;
   margin-top: 100px;
+}
+.welcome h2{
+  font-size: 3em;
+}
+.welcome button{
+  margin: 30px auto;
 }
 </style>
